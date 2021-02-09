813934954465b9da9a9e7dc6bad9c739587dfe27
Yamaya <fuwafuwa41.glr@gmail.com>
2021-02-09 14:37:17 +0000

    Add view

--- config/application.rb
+++ config/application.rb
@@ -6,14 +6,21 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+#config.middleware.use ActionDispatch::flash
+
 module TrainingApp4
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Railsアプリデフォルトのタイムゾーン(default 'UTC')
+    # TimeZoneList: http://api.rubyonrails.org/classes/ActiveSupport/TimeZone.html
+    config.time_zone = ENV["TZ"]
+
+    # i18nで使われるデフォルトのロケールファイルの指定(default :en)
+    config.i18n.default_locale = :ja
+
+    config.api_only = true
+    config.middleware.use ActionDispatch::Flash
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get 'admin/users/new', to: 'admin/users#new'
+  get 'admin/users/edit', to: 'admin/users#edit'
+  #post 'admin/users/new', to: 'admin/users#new'
+  #post 'admin/users/edit', to: 'admin/users#edit'
+
+
   namespace :admin do
     resources :users
   end
 
-  root to:'users#index'
+  #root to:'/login'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
