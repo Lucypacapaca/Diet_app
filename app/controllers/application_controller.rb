a1ae97d53f7e2abacd02ad0485f38c90aa854995
Lucypacapaca <fuwafuwa41.glr@gmail.com>
2021-04-17 08:44:27 +0000

    round1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,58 +9,61 @@ class ApplicationController < ActionController::Base
 
 
 
-    def bm_male
-        if @user.activity_level == '1'
-            ( (10 * @user.body_mass) + (6.25 * @user.tall) - (5 * @user.age) + 5 ) * 1.725
-        elsif @user.activity_level == '2'
-            ( (10 * @user.body_mass) + (6.25 * @user.tall) - (5 * @user.age) + 5 ) * 1.55
-        else
-            ( (10 * @user.body_mass) + (6.25 * @user.tall) - (5 * @user.age) + 5 ) * 1.2
-        end
-    end
-
-    def bm_female
-        if @user.activity_level == '1'
-            ( (10 * @user.body_mass) + (6.25 * @user.tall) - (5 * @user.age) - 161 ) * 1.725
-        elsif @user.activity_level == '2'
-            ( (10 * @user.body_mass) + (6.25 * @user.tall) - (5 * @user.age) - 161 ) * 1.55
-        else
-            ( (10 * @user.body_mass) + (6.25 * @user.tall) - (5 * @user.age) - 161 ) * 1.2
-        end
-    end
-
-    def require_kcal
-        if @user.sex == 1
-            if @user.purpose == '1'
-                bm_male * 1.2
-            elsif @user.purpose == '2'
-                bm_male * 1
-            else 
-                bm_male * 0.8
-            end
-        else
-            if @user.purpose == '1'
-                bm_female * 1.2
-            elsif @user.purpose == '2'
-                bm_female * 1
-            else 
-                bm_female * 0.8
+    unless  :tall.blank? || :age.blank? || :body_mass.blank? || :sex.blank? || :activity_level.blank? || :purpose.blank?
+        def bm_male
+            if @current_user.activity_level== '1'
+                ( (10 * @current_user.body_mass) + (6.25 * @current_user.tall) - (5 * @current_user.age) + 5 ) * 1.725
+            elsif @current_user.activity_level == '2'
+                ( (10 * @current_user.body_mass) + (6.25 * @current_user.tall) - (5 * @current_user.age) + 5 ) * 1.55
+            else
+                ( (10 * @current_user.body_mass) + (6.25 * @current_user.tall) - (5 * @current_user.age) + 5 ) * 1.2
             end
         end
-    end
 
-    def require_protein
-        @user.body_mass * 2
-    end
+        def bm_female
+            if @current_user.activity_level == '1'
+                ( (10 * @current_user.body_mass) + (6.25 * @current_user.tall) - (5 * @current_user.age) - 161 ) * 1.725
+            elsif @current_user.activity_level == '2'
+                ( (10 * @current_user.body_mass) + (6.25 * @current_user.tall) - (5 * @current_user.age) - 161 ) * 1.55
+            else
+                ( (10 * @current_user.body_mass) + (6.25 * @current_user.tall) - (5 * @current_user.age) - 161 ) * 1.2
+            end
+        end
 
-    def require_fat
-        require_kcal * 0.25 / 9
-    end
+        def require_kcal
+            if @current_user.sex == 1
+                if @current_user.purpose == '1'
+                    bm_male * 1.2
+                elsif @current_user.purpose == '2'
+                    bm_male * 1
+                else 
+                    bm_male * 0.8
+                end
+            else
+                if @current_user.purpose == '1'
+                    bm_female * 1.2
+                elsif @current_user.purpose == '2'
+                    bm_female * 1
+                else 
+                    bm_female * 0.8
+                end
+            end
+        end
 
-    def require_carbon_hydrate
-        (require_kcal - (require_protein * 4) - (require_fat * 9) ) / 4
-    end
+        def require_protein
+            #current_user.@current_user.body_mass * 2
+            @current_user.body_mass * 2
+        end
 
+        def require_fat
+            require_kcal * 0.25 / 9
+        end
+
+        def require_carbon_hydrate
+            (require_kcal - (require_protein * 4) - (require_fat * 9) ) / 4
+        end
+    end
+    
 
     private
 
@@ -72,21 +75,21 @@ class ApplicationController < ActionController::Base
         redirect_to login_url unless current_user
     end
 
-    def cal_proten
-        @cal_proten = :protein * :amount / 100
-    end
+    # def cal_proten
+    #     @cal_proten = :protein * :amount / 100
+    # end
 
-    def cal_fat
-        @cal_fat = :fat * :amount / 100
-    end
+    # def cal_fat
+    #     @cal_fat = :fat * :amount / 100
+    # end
 
-    def cal_carbon_hydrate
-        @cal_carbon_hydrate = :carbon_hydrate * 100 / :amount
-    end
+    # def cal_carbon_hydrate
+    #     @cal_carbon_hydrate = :carbon_hydrate * 100 / :amount
+    # end
 
-    def cal_kcal
-        @cal_kcal = cal_proten*4 + cal_fat*9 + cal_carbon_hydrate*4
-    end
+    # def cal_kcal
+    #     @cal_kcal = cal_proten*4 + cal_fat*9 + cal_carbon_hydrate*4
+    # end
 
     
 
